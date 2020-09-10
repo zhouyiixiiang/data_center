e93b671592895cfe0945a65cfbf338ea77355083
yiixiiang <casit>
2020-09-10 18:10:45 +0800

    add new function to run npm

--- config/index.js
+++ config/index.js
@@ -6,15 +6,23 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
-    // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
-
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {
+      '/backend':{
+        target: "http://127.0.0.1:8000",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/backend': '/backend' //这里理解成用‘/api’代替target里面的地址，组件中我们调接口时直接用/api代替
+              // 比如我要调用'http://0.0:300/user/add'，直接写‘/api/user/add’即可 代理后地址栏显示
+        }
+      }
+    },
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
